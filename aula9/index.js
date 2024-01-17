d4531f1816f83a23459181d361d329065aade5d3
erickdavid14 <erickdavid393@gmail.com>
2024-01-16 22:33:34 -0300

    att

--- aula9/index.js
+++ aula9/index.js
@@ -3,8 +3,8 @@ const nome1 = "Emily"; // String
 const nome2 = `David`; // String
 const num1 = 10; //number
 const num2 = 10.50; //number
-let nomeAluno; // undefined -> Nao aponta pra local nem um na memoria
-const sobrenomeAluno =  null; //nulo -> nao aponta pra local nem um na memoria
+let nomeAluno; // undefined -> Nao aponta pra local nenhum  na memoria
+const sobrenomeAluno =  null; //nulo -> nao aponta pra local nenhum  na memoria
 const aprovado = false; //boolean = true, false (dados logicos)
 
 
