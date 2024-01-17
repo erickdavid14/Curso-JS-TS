a3098fa2a0efc19bed23f2e7c0a7ce51ef9148b0
erickdavid14 <erickdavid393@gmail.com>
2024-01-16 22:31:28 -0300

    att

---
+++ aula9/index.js
@@ -0,0 +1,15 @@
+const nome = 'Erick'; // String
+const nome1 = "Emily"; // String
+const nome2 = `David`; // String
+const num1 = 10; //number
+const num2 = 10.50; //number
+let nomeAluno; // undefined -> Nao aponta pra local nem um na memoria
+const sobrenomeAluno =  null; //nulo -> nao aponta pra local nem um na memoria
+const aprovado = false; //boolean = true, false (dados logicos)
+
+
+let a = [1, 2];
+const b = a;
+console.log(a, b)
+a = 3;
+console.log(a, b)
