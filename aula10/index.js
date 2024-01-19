922e9f73b14792b6cff840499405913bf8d930c0
erickdavid14 <erickdavid393@gmail.com>
2024-01-19 19:50:19 -0300

    att

--- aula10/index.js
+++ aula10/index.js
@@ -3,6 +3,6 @@ let num2 = Number('5');
 const nome = 'tenho';
 
 console.log(`Eu ${nome} ${(num1 + num2)} maçãs`);
-num2 = '5'
+num2 = '45'
 
-console.log(`Eu ${nome} ${(num1 + num2)} maçãs`)
+console.log(`Eu ${nome} ${num2} maçãs`)
