17a2bd61a5624122a24591bfa4ea9938138e50b8
erickdavid14 <erickdavid393@gmail.com>
2024-02-27 20:08:15 -0300

    att

--- aula16/index.js
+++ aula16/index.js
@@ -1,7 +1,9 @@
 
 //               0        1          2        indices de array
 const alunos = ['Erick', 'Helton','Heloisa'];
-alunos.shift();
-console.log(alunos);
+alunos.unshift('Heleno');
+alunos.push('Erivoneide');
+
+console.log(alunos.slice(2,4));
 
 
--- aula17/index.js
+++ aula17/index.js
@@ -1,4 +1,13 @@
-const raiz = n => n ** 0.5;
+const raiz = function(n){
+    return n ** 0.5;
+};
+console.log(raiz(9)); 
 
-console.log(raiz(9))
-    
+/*const raiz = n => n ** 0.5;
+console.log(raiz(9)); */
+
+/*function soma(x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+}
+console.log(soma(5,5)); */
