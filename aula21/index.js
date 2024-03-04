ccb54068ad52c5d919d2a4d58d56ed4d71b332f3
erickdavid14 <erickdavid393@gmail.com>
2024-03-04 20:01:41 -0300

    att

---
+++ aula21/index.js
@@ -0,0 +1,15 @@
+/*
+operadores de comparação
+> maior que
+>= mairo que ou igual
+< menor que
+<= menor que ou igual
+== igualdade (valor) *************
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ***********
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = 12;
+const comp = num1 !== num2;
+console.log(comp);
---
+++ aula22/index.js
@@ -0,0 +1,15 @@
+/* 
+operadores logicos
+&& -> AND -> E todas as expressões presicam ser verdadeiras para retorna true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && false && true;
+// const expressaoOr = false || false || true;
+
+// const usuario = 'Erick';
+// const senha = 123456;
+// const vaiLogar = usuario === 'Erick' && senha === 123456;
+
+
+console.log(!true)
