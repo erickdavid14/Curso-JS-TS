3ac4f2ac28ee8f2b742ddbeddb11a72d95b9c9f0
erickdavid14 <erickdavid393@gmail.com>
2024-03-12 18:08:42 -0300

    att

---
+++ aula25/index.js
@@ -0,0 +1,14 @@
+const numero = 20;
+
+
+// IF -> se (numero >= 0 && numero <=5 ) ocrorrer , faça osso {codigo aqui}
+// ELSE -> se nao {aqui vai o codigo}
+if (numero >= 0 && numero <=5 ) {
+    console.log('O numero esta entre 0 e 5.');
+} else if (numero >=6 && numero <=8 ) {
+    console.log('O numero esta entre 6 e 8.');
+} else if (numero >=9 && numero <=11) {
+    console.log('O numero esta entre 9 e 11.');
+} else {
+    console.log('O numero nao esta entre 0 e 11.')
+}
