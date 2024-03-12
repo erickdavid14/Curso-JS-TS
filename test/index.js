dc2a499d64a7d7eae518bd8067e4a63ca8472297
erickdavid14 <erickdavid393@gmail.com>
2024-03-12 18:20:08 -0300

    att

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
-const pessoa = {
-        nome:'Erick',
-        sobrenome:'David',
-};
-console.log(pessoa.nome, pessoa.sobrenome);
- 
+function criaPessoa (nome, sobrenome, idade) {
+        return {nome, sobrenome, idade};
+}
+const pessoa1 = criaPessoa('Erick','David','23');
+console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade);
