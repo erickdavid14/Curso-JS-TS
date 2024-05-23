817bb6362eef34186c3a3c64705355fce697b0e2
erickdavid14 <erickdavid393@gmail.com>
2024-05-23 18:51:25 -0300

    att

---
+++ aula41ex/index.js
@@ -0,0 +1,16 @@
+/* function conta(x, y) {
+    return x > y ? x : y;
+
+     if (x > y){
+        return x;
+    } 
+     return y; 
+}  */
+/* const max2= (x, y) => {
+    return x > y ? x : y;
+}; */
+
+const max2= (x, y) => x > y ? x : y;
+    
+
+console.log(max2(100,89));
