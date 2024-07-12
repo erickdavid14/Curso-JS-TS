4fb6ca2913420926422e7b4f7663006c155dc183
erickdavid14 <erickdavid393@gmail.com>
2024-07-12 17:49:44 -0300

    att

---
+++ aula49/index.js
@@ -0,0 +1,17 @@
+// Declaração de função
+function falaOi (){
+    console.log('oi');
+}
+falaOi()
+// função de primeira class
+
+const souUmDado = function() {
+    console.log('sou um dado');
+};
+souUmDado();
+
+// arrow function
+const arrowFunction = () => {
+    console.log('sou uma arrowFunction');
+};
+arrowFunction();
