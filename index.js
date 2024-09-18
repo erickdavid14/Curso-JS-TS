b70dfff2ebdebb2c650b359bcac8af7dd721c4a6
erickdavid14 <erickdavid393@gmail.com>
2024-09-18 18:04:39 -0300

    att

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /* function conta(x, y){
     return x > y ? x : y;
 }
-const numeros = conta(25, 25)
+const numeros = conta(8, 7)
 console.log(numeros); */
 
 // FizzBuzz
-const resultado = fizzBuzz(20);
+/* const resultado = fizzBuzz(20);
 console.log(resultado);
 function fizzBuzz(entrada){
  if (typeof entrada !== 'number')
@@ -18,4 +18,38 @@ if (entrada % 5 === 0)
     return 'Buzz';
 if (entrada % 3 !== 0 && entrada % 5 !== 0 )
     return entrada;
-}
+} */
+
+// Objeto
+
+/* const pessoa = {
+    nome: 'Erick',
+    sobrenome: 'David',
+    idade: 25,
+    cidade: 'Agrestina'
+};
+console.log(typeof pessoa, pessoa.nome); */
+
+// Array
+/*  const idadeFamilia = [23,45,67,21];
+ function mudaIndice(ind){
+    idadeFamilia[3] = ind ;
+ };
+console.log(idadeFamilia[3]); 
+mudaIndice(50);
+console.log(idadeFamilia[3]); */
+
+// function
+
+/* let nomePessoa = 'Erick';
+function mudaNome(nome, sobrenome){
+    nomePessoa = nome + ' ' + sobrenome;
+};
+console.log(nomePessoa);
+mudaNome('Emily', 'Ramos');
+console.log(nomePessoa); */
+
+// tipos de funçao
+
+
+
